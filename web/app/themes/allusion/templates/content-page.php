99387ff9ad660b41de5d8b5b8e62cf939ee36fed
Paul Tibbetts <email@paultibbetts.uk>
2015-07-12 14:21:19 +0100

    Added e-content to page template

--- web/app/themes/allusion/templates/content-page.php
+++ web/app/themes/allusion/templates/content-page.php
@@ -1,2 +1,4 @@
-<?php the_content(); ?>
+<div class="e-content">
+  <?= the_content(); ?>
+</div>
 <?php wp_link_pages(['before' => '<nav class=""><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
