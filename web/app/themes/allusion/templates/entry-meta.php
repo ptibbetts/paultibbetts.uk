31d8fc0d551496162e659fe3de72ea7ceba82e7d
Paul Tibbetts <email@paultibbetts.uk>
2015-07-12 15:10:30 +0100

    Fixed author data in entry-meta

--- web/app/themes/allusion/templates/entry-meta.php
+++ web/app/themes/allusion/templates/entry-meta.php
@@ -2,7 +2,7 @@
 <p>Permalink: <a href="<?= get_permalink();?>" class="u-url"><?= get_permalink();?></a></p>
 <p class="byline author vcard hcard h-card p-author" itemtype="https://schema.org/Person" itemscope="" itemprop="author"><?= __('By', 'sage'); ?>
   <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="url u-url fn p-name" itemprop="url" title="<?= get_the_author(); ?>">
-    <?php $avatar = get_avatar( 'get_the_author', 20 ); ?>
-    <img class="u-photo" src="<?= $avatar; ?>" alt="<?= get_the_author(); ?>"/>
+    <img class="u-photo" src="<?php get_blavatar(20); ?>" alt=""/>
+    <span itemprop="name"><?= get_the_author(); ?></span>
   </a>
 </p>
