f16140b9bb8acb4c684e25edc02f6cdca57df75e
Paul Tibbetts <email@paultibbetts.uk>
2015-07-12 15:07:49 +0100

    Fixed get_the_author function

--- web/app/themes/allusion/templates/entry-meta.php
+++ web/app/themes/allusion/templates/entry-meta.php
@@ -3,6 +3,6 @@
 <p class="byline author vcard hcard h-card p-author" itemtype="https://schema.org/Person" itemscope="" itemprop="author"><?= __('By', 'sage'); ?>
   <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="url u-url fn p-name" itemprop="url" title="<?= get_the_author(); ?>">
     <?php $avatar = get_avatar( 'get_the_author', 20 ); ?>
-    <img class="u-photo" src="<?= $avatar; ?>" alt="<?= get_the_author; ?>"/>
+    <img class="u-photo" src="<?= $avatar; ?>" alt="<?= get_the_author(); ?>"/>
   </a>
 </p>
