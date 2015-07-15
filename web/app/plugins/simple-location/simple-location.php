b234732d80590c4e73c6bc4665f5040fc595e684
Paul Tibbetts <email@paultibbetts.uk>
2015-07-15 22:25:20 +0100

    Swapped Social for Swap and added Simple Location

---
+++ web/app/plugins/simple-location/simple-location.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: Simple Location
+ * Plugin URI: https://wordpress.org/plugins/simple-location/
+ * Description: Adds Location to Wordpress Pages and Posts. 
+ * Version: 2.1.0
+ * Author: David Shanske
+ * Author URI: https://david.shanske.com
+ */
+
+define ("SIMPLE_LOCATION_VERSION", "2.1.0");
+
+// Map Provider Interface
+require_once( plugin_dir_path( __FILE__ ) . 'includes/interface-map-provider.php');
+
+// Map Providers
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-osm-static.php');
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-google-map-static.php');
+
+// Configuration Functions
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-loc-config.php');
+
+// Add Location Post Meta
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-loc-postmeta.php');
+
+// Add Location Display Functions
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-loc-view.php');
+
+// Timezone Display Functions
+require_once( plugin_dir_path( __FILE__ ) . 'includes/class-post-timezone.php');
+
+if (!function_exists('ifset') ) {
+  function ifset(&$var, $default = false) {
+      return isset($var) ? $var : $default;
+  }
+}
