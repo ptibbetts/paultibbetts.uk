e579b34d4a96e2c89abcfac9d0b72ee0d2ea3bb4
Paul Tibbetts <email@paultibbetts.uk>
2016-01-31 17:37:06 +0000

    Added image template

---
+++ site/web/app/themes/allusion/templates/content-image.php
@@ -0,0 +1,33 @@
+<article <?php post_class(); ?>>
+
+  <div class="entry-summary">
+    <?php the_content(); ?>
+  </div>
+
+  <footer>
+    <?php get_template_part('templates/entry-meta'); ?>
+
+    <?php
+
+      $instagram_filter = get_metadata('post',$post->ID, 'instagram_filter', true);
+
+      if ( $instagram_filter != null ) {
+          echo '<p>';
+          echo 'Filtered with: ' . $instagram_filter;
+          echo '</p>';
+      }
+
+      $instagram_url = get_metadata('post',$post->ID, 'instagram_url', true);
+
+      if ( $instagram_url != null ) {
+          echo '<a rel="syndication" href="'.$instagram_url.'">';
+          echo 'View on Instagram';
+          echo '</a>';
+      }
+      
+    ?>
+
+
+  </footer>
+
+</article>
