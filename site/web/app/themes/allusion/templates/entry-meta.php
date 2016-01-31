105a54682f3152449a6d83fc1923eef93af29feb
Paul Tibbetts <email@paultibbetts.uk>
2016-01-31 17:46:58 +0000

    Added permalink to entry meta

--- site/web/app/themes/allusion/templates/entry-meta.php
+++ site/web/app/themes/allusion/templates/entry-meta.php
@@ -1,2 +1,7 @@
-<time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+<a href="<?php the_permalink(); ?>">
+  <time class="updated" datetime="<?= get_post_time('c', true); ?>">
+    <?= get_the_date(); ?>
+  </time>
+</a>
+
 <p class="byline author vcard"><?= __('By', 'sage'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a></p>
